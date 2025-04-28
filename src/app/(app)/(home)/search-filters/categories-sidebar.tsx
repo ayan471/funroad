883461760362fc458fa3817c776414433817a404
ayan471 <pakhiraayan@gmail.com>
2025-04-28 19:54:58 +0530

    06: categories finalization

---
+++ src/app/(app)/(home)/search-filters/categories-sidebar.tsx
@@ -0,0 +1,100 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { CustomCategory } from "../types";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+import { ChevronLeft, ChevronRightIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  data: CustomCategory[];
+}
+
+export const CategoriesSidebar = ({ open, onOpenChange, data }: Props) => {
+  const router = useRouter();
+  const [parentCategories, setParenntCategories] = useState<
+    CustomCategory[] | null
+  >(null);
+  const [selectCategory, setSelectedCategory] = useState<CustomCategory | null>(
+    null
+  );
+
+  const currentCategories = parentCategories ?? data ?? [];
+
+  const handleOpenChange = (open: boolean) => {
+    setSelectedCategory(null);
+    setParenntCategories(null);
+    onOpenChange(open);
+  };
+
+  const handleCategoryClick = (category: CustomCategory) => {
+    if (category.subcategories && category.subcategories.length > 0) {
+      setParenntCategories(category.subcategories as CustomCategory[]);
+      setSelectedCategory(category);
+    } else {
+      if (currentCategories && selectCategory) {
+        router.push(`/${selectCategory.slug}/${category.slug}`);
+      } else {
+        if (category.slug === "all") {
+          router.push("/");
+        } else {
+          router.push(`/${category.slug}`);
+        }
+      }
+
+      handleOpenChange(false);
+    }
+  };
+
+  const backgroundColor = selectCategory?.color || "white";
+
+  const handleBackClick = () => {
+    if (parentCategories) {
+      setParenntCategories(null);
+      setSelectedCategory(null);
+    }
+  };
+
+  return (
+    <Sheet open={open} onOpenChange={handleOpenChange}>
+      <SheetContent
+        side="left"
+        className="p-0 transition-none"
+        style={{ backgroundColor }}
+      >
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Categories</SheetTitle>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {parentCategories && (
+            <button
+              onClick={handleBackClick}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium cursor-pointer"
+            >
+              <ChevronLeft className="size-4 mr-2" />
+              Back
+            </button>
+          )}
+          {currentCategories.map((category) => (
+            <button
+              key={category.slug}
+              onClick={() => handleCategoryClick(category)}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center text-base font-medium cursor-pointer"
+            >
+              {category.name}
+              {category.subcategories && category.subcategories.length > 0 && (
+                <ChevronRightIcon className="size-4" />
+              )}
+            </button>
+          ))}
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
