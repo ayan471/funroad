64c2251eda5969504dfb894b056ef83cffc6f5f7
ayan471 <pakhiraayan@gmail.com>
2025-05-15 12:36:41 +0530

    21: library

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,10 +5,13 @@ import { productsRouter } from "@/modules/products/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
 import { checkoutRouter } from "@/modules/checkout/server/procedures";
+import { libraryRouter } from "@/modules/library/server/procedures";
+
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   products: productsRouter,
   tenants: tenantsRouter,
+  library: libraryRouter,
   checkout: checkoutRouter,
   tags: tagsRouter,
   categories: categoriesRouter,
